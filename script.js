1d7b3741738ea6fd123168b0465988e3b90476f3
Karen <ovakimyankaren325551@gmail.com>
2024-03-07 13:17:42 +0200

    deleted comments

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function calcAge(ageInYears) {
     return ageInDays;
   }
   
-  // Examples
+ 
   console.log(calcAge(65)); // ➞ 23725
   console.log(calcAge(0));  // ➞ 0
   console.log(calcAge(20)); // ➞ 7300
